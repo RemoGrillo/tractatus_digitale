d34899eb2e4079de2c4b4870be0c784c37c911b5
Remo Grillo <remogrillo@MacBook-Pro-di-Remo-16.local>
2020-11-15 16:37:39 +0100

    Added sidebar and languages

--- main.js
+++ main.js
@@ -10,6 +10,38 @@ $(document).ready(function(){
         clickOnProposition($(this))
     });
 
+    $('.content_en').show();
+    $('.content_de').hide();
+
+    var switchButton 			= document.querySelector('.switch-button');
+    var switchBtnRight 			= document.querySelector('.switch-button-case.right');
+    var switchBtnLeft 			= document.querySelector('.switch-button-case.left');
+    var activeSwitch 			= document.querySelector('.active');
+
+    function switchLeft(){
+        switchBtnRight.classList.remove('active-case');
+        switchBtnLeft.classList.add('active-case');
+        activeSwitch.style.left 						= '0%';
+        $('.content_en').show();
+        $('.content_de').hide();
+    }
+
+    function switchRight(){
+        switchBtnRight.classList.add('active-case');
+        switchBtnLeft.classList.remove('active-case');
+        activeSwitch.style.left 						= '50%';
+        $('.content_en').hide();
+        $('.content_de').show();
+    }
+
+    switchBtnLeft.addEventListener('click', function(){
+        switchLeft();
+    }, false);
+
+    switchBtnRight.addEventListener('click', function(){
+        switchRight();
+    }, false);
+
 });
 
 function clickOnProposition(clicked){
